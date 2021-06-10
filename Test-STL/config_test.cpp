ab0c606416c0849bb2f049aed4a2511e5e86acea
zzb610 <bwyuchi@163.com>
2021-06-10 17:30:01 +0800

    开始学习

---
+++ Test-STL/config_test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../SGI_STL/vector"
+
+using namespace sgistd;
+int main(int argc, char const *argv[])
+{
+    std::cout << "hello" << std::endl;
+    vector<int> v;
+    return 0;
+}
